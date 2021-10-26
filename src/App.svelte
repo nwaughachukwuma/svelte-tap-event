26b086ae2c09e9f1bb7ba61a4db17c29f5a2c42d
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-10-26 23:45:24 +0400

    create svelte app

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import touchEvents from "./lib";
+
+  const touchEnter = () => {
+    console.log("touchenter");
+  };
+  const touchLeave = () => {
+    console.log("touchleave");
+  };
+  const tap = () => {
+    console.log("tap");
+  };
+</script>
+
+<app
+  use:touchEvents
+  on:touchenter={touchEnter}
+  on:touchleave={touchLeave}
+  on:tap={tap}
+>
+  <div>Hello world!</div>
+</app>
+
+<style>
+  app div {
+    border: 1px solid red;
+    padding: 10px;
+    margin: auto;
+  }
+</style>
