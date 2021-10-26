8e0aefbbe78af1d6dac3d3b80d87ccaac7316986
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-10-26 23:43:14 +0400

    refine svelte-action

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,5 @@
-let hovering = false;
-let tap = false;
 const touchSpot: { x?: number; y?: number } = {};
 const TOUCH_POINT_BUFFER = 10;
-
-type Param = {
-  hovering: boolean;
-  tap: boolean;
-};
 
 export default function touchEvents(node: HTMLElement) {
   node.addEventListener("pointerenter", pointerEnterHandler);
@@ -14,10 +7,6 @@ export default function touchEvents(node: HTMLElement) {
   node.addEventListener("click", clickHandler);
 
   return {
-    update(fn: (param: Param) => void) {
-      fn({ hovering, tap });
-    },
-
     destroy() {
       node.removeEventListener("pointerenter", pointerEnterHandler);
       node.removeEventListener("pointerleave", pointerLeaveHandler);
@@ -28,32 +17,29 @@ export default function touchEvents(node: HTMLElement) {
 
 function pointerEnterHandler(e: PointerEvent) {
   e.preventDefault();
-  hovering = true;
-  tap = false;
-
   touchSpot.x = e.x;
   touchSpot.y = e.y;
+
+  e.target?.dispatchEvent(new CustomEvent("touchenter"));
 }
 
 function pointerLeaveHandler(e: PointerEvent) {
   e.preventDefault();
-  hovering = false;
-  tap = false;
-
   if (e.pointerType === "touch") {
     if (touchSpot.x && touchSpot.y) {
       if (
         Math.abs(touchSpot.x - e.x) < TOUCH_POINT_BUFFER &&
         Math.abs(touchSpot.y - e.y) < TOUCH_POINT_BUFFER
       ) {
-        clickHandler(e);
+        return clickHandler(e);
       }
     }
   }
+
+  e.target?.dispatchEvent(new CustomEvent("touchleave"));
 }
 
 function clickHandler(e: Event) {
   e.preventDefault();
-  hovering = false;
-  tap = true;
+  e.target?.dispatchEvent(new CustomEvent("tap"));
 }
